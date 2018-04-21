e339f04d7010b5d479b187e4ba96e0043bbf8a9d
Vitaly Karpenko <vitkarpenko@gmail.com>
2018-04-21 20:17:48 +0300

    Works!

--- twiccord/twiccord.py
+++ twiccord/twiccord.py
@@ -1,25 +1,95 @@
+import asyncio
 import os
-import threading
 
+from bs4 import BeautifulSoup
 import discord
 import twitter
+import requests
 
 TOKENS = dict(
     CONSUMER_KEY=os.getenv('TWICCORD_TWITTER_CONSUMER_KEY'),
     CONSUMER_SECRET=os.getenv('TWICCORD_TWITTER_CONSUMER_SECRET'),
     ACCESS_TOKEN_KEY=os.getenv('TWICCORD_TWITTER_ACCESS_TOKEN_KEY'),
-    ACCESS_TOKEN_SECRET=os.getenv('TWICCORD_TWITTER_ACCESS_TOKEN_SECRET')
+    ACCESS_TOKEN_SECRET=os.getenv('TWICCORD_TWITTER_ACCESS_TOKEN_SECRET'),
+    BOT_TOKEN=os.getenv('TWICCORD_DISCORD_BOT_TOKEN')
 )
 
 
-class TwitterListener:
+class KindleQuotesRedirecter(discord.Client):
     """ Periodically checks user's timeline for new tweets with amazon kindle quotes.
+    Reposts new quotes to Discord channel.
+
+    self.tweets is a list of last fetched tweets.
     """
     def __init__(self, tokens, username):
-        self.api = twitter.Api(
+        super().__init__()
+        self.loop = asyncio.get_event_loop()
+        self.twitter = twitter.Api(
             consumer_key=tokens['CONSUMER_KEY'],
-            consumer_secret=['CONSUMER_SECRET'],
-            access_token_key=['ACCESS_TOKEN_KEY'],
-            access_token_secret=['ACCESS_TOKEN_SECRET']
+            consumer_secret=tokens['CONSUMER_SECRET'],
+            access_token_key=tokens['ACCESS_TOKEN_KEY'],
+            access_token_secret=tokens['ACCESS_TOKEN_SECRET']
         )
-        self.latest_tweets = list()
+        self.username = username
+        self.tweets = self.fetch_latest_tweets()
+
+    def fetch_updates_ids(self):
+        """ Получает последние твиты, записывает их в self.tweets
+        и возвращает ID новых твитов.
+        """
+        latest_tweets = self.fetch_latest_tweets()
+        updates_ids = (
+            set(tweet.id for tweet in self.fetch_latest_tweets())
+            - set(tweet.id for tweet in self.tweets)
+        )
+        self.tweets = latest_tweets
+        return updates_ids
+
+    def fetch_latest_tweets(self):
+        return list(
+            self.twitter.GetUserTimeline(
+                screen_name=self.username,
+                count=50
+            )
+        )
+
+    def find_kindle_quotes(self, message):
+        text = message.text
+        last_word = text.split()[-1]
+        if not last_word.startswith('https://t.co/'):
+            return None
+        r = requests.get(last_word)
+        parser = BeautifulSoup(r.text, 'lxml')
+        quote = parser.find(id='kp-quote')
+        if quote:
+            return quote.string.strip()
+        else:
+            return None
+
+    def format_quote(self, quote):
+        return f'**Виталий цитирует**:\n_{quote}_'
+
+    async def post_updates_to_discord(self):
+        books = self.get_channel('405339907012427779')
+        updates_ids = self.fetch_updates_ids()
+        updates = (tweet for tweet in self.tweets if tweet.id in updates_ids)
+        for update in updates:
+            quote = self.find_kindle_quotes(update)
+            if quote:
+                await self.send_message(
+                    books,
+                    self.format_quote(quote)
+                )
+
+    async def _run_periodically(self, period, method):
+        while True:
+            await method()
+            await asyncio.sleep(period)
+
+    async def on_ready(self):
+        await self._run_periodically(10, self.post_updates_to_discord)
+
+
+if __name__ == '__main__':
+    redirecter = KindleQuotesRedirecter(TOKENS, 'karpenko_vitaly')
+    redirecter.run(TOKENS['BOT_TOKEN'])
