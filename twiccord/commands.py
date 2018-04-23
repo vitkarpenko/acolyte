11254bdda5a26e4a59f9d53f14895a128d0bf6d8
Vitaly Karpenko <vitkarpenko@gmail.com>
2018-04-23 22:15:49 +0300

    Try this?

--- twiccord/commands.py
+++ twiccord/commands.py
@@ -3,11 +3,12 @@ import asyncio
 from discord.ext import commands
 
 @commands.command()
-async def roll(self, dice):
+async def roll(dice):
+    nonlocal redirecter
     try:
         rolls, limit = map(int, dice.split('d'))
     except Exception:
-        await self.bot.say('**XdY**, просил же!')
+        await redirecter.say('**XdY**, просил же!')
     rolls = [random.randint(1, limit) for r in range(rolls)]
     comma_separated_rolls = ', '.join(str(roll) for roll in rolls)
-    return self.bot.say(f'**Выпало**: {comma_separated_rolls}\n**Сумма**: {sum(rolls)}')
+    return redirecter.say(f'**Выпало**: {comma_separated_rolls}\n**Сумма**: {sum(rolls)}')
