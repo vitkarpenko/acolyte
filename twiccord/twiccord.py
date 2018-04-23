66df5b54f49b7a248f8303948279167981493b56
Vitaly Karpenko <vitkarpenko@gmail.com>
2018-04-23 22:54:55 +0300

    Debug

--- twiccord/twiccord.py
+++ twiccord/twiccord.py
@@ -92,17 +92,16 @@ class KindleQuotesRedirecter(commands.Bot):
     async def on_ready(self):
         await self._run_periodically(10, self.post_updates_to_discord)
 
-
-@commands.command()
-async def roll(dice):
-    global redirecter
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await redirecter.say('**XdY**, просил же!')
-    rolls = [random.randint(1, limit) for r in range(rolls)]
-    comma_separated_rolls = ', '.join(str(roll) for roll in rolls)
-    return redirecter.say(f'**Выпало**: {comma_separated_rolls}\n**Сумма**: {sum(rolls)}')
+    @commands.command()
+    async def roll(self, dice):
+        global redirecter
+        try:
+            rolls, limit = map(int, dice.split('d'))
+        except Exception:
+            await self.say('**XdY**, просил же!')
+        rolls = [random.randint(1, limit) for r in range(rolls)]
+        comma_separated_rolls = ', '.join(str(roll) for roll in rolls)
+        return self.say(f'**Выпало**: {comma_separated_rolls}\n**Сумма**: {sum(rolls)}')
 
 
 def main():
